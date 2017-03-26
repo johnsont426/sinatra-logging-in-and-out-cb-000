a824bd6fbf23247bb7bd3bb3096a40cc6c807a6a
Chuan-Sheng Tai <johnsont426@gmail.com>
2017-03-26 19:12:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,32 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    User.create(username: "johnson", password: "haha")
     erb :index
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if !!@user
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if session.has_key?(:user_id)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
 end
-
